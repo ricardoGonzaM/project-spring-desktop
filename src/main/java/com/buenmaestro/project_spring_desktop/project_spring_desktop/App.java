38791075dd5e1552ac9911ebfd1ac48084d13063
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2023-02-27 20:50:22 -0600

    Version inicial del proyecto project-spring-desktop

---
+++ src/main/java/com/buenmaestro/project_spring_desktop/project_spring_desktop/App.java
@@ -0,0 +1,13 @@
+package com.buenmaestro.project_spring_desktop.project_spring_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
